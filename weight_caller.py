3717da38d27a79e6921f8ccf92362369110519f5
rditljeh <117112638+rditljeh@users.noreply.github.com>
2023-01-11 15:09:12 -0600

    Creating directory for interpolated csvs

--- weight_caller.py
+++ weight_caller.py
@@ -5,6 +5,9 @@ from datetime import datetime
 #specify the directory the gribs are from
 csv_files = os.listdir("grib_csvs/")
 
+if not os.path.exists("Transformed_Gribs"):
+   os.makedirs("Transformed_Gribs")
+
 for csv in csv_files:
     if not os.path.exists("Transformed_Gribs/"+csv.split("-grib")[0]+"_reformatted"):
         print("Starting conversion of: ", csv, " at ", datetime.now())
